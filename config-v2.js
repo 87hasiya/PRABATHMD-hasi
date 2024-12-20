ff9bcf32e3504428a2fba228b1dd5dbf4d562c97
87hasiya <ravimd036@gmail.com>
2024-12-20 21:28:42 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
 const GITHUB_AUTH_TOKEN = "null"
 const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const SESSION_ID = "PRABATH-MD~zJ9GVDAY#HP2DxFcXq9ocB44V_HaOR9bilqVzWnPwhzjcP65hs6w"
 
 
 module.exports = {
